9e1da4d0442e01a5724def6db5fc7429a0c9c425
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-02-06 09:34:11 +0100

    NewsFeed erweitert

--- src/theSocialNetwork/NewsFeed.java
+++ src/theSocialNetwork/NewsFeed.java
@@ -7,20 +7,30 @@ public class NewsFeed {
     private ArrayList<Submission> submission;
     
     public NewsFeed() {
-    	this.submission = new ArrayList();
+    	this.submission = new ArrayList<>();
     }
     
-    void addSubmission (Submission submission) {
+    /*
+     * Ads a Submission
+     */
+    public void addSubmission (Submission submission) {
     	this.submission.add(submission);
     }
     
-    void printAllSubmission() {
+    /*
+     * Prints all submission
+     */
+    public void printAllSubmission() {
     	for (Submission s : submission) {
     		System.out.println(s);
     	}
     }
     
-    void printSubmissionByUser(String userName) {
+    
+    /*
+     * Prints all submission by user
+     */
+    public void printSubmissionByUser(String userName) {
     	for (Submission s : submission) {
     		if (s.getUsername().equals(userName)) {
     			System.out.println(s);
@@ -28,9 +38,24 @@ public class NewsFeed {
     	}
     }
     
-    void deleteSubmission(Submission submission) {
+    /*
+     * Deletes a submission
+     */
+    public void deleteSubmission(Submission submission) {
     	this.submission.remove(submission);
     }
     
-    
+   /*
+    *  Finds a user in class submission from her or his post
+    */
+   ArrayList<Submission> findUser(String userName, boolean onlyPhotos) {
+	   ArrayList<Submission> result = new ArrayList<>();
+	   for (Submission s : submission) {
+		   if (s.getUsername().equals(userName) && (!onlyPhotos || s instanceof Photo)) {
+			   result.add(s);
+		   }
+	   }
+	   System.out.println("Die User " + result);
+	   return result;
+   }
 }
