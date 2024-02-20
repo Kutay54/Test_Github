824f14d21d97adfc0d20a317ba6130aedf4e7708
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-02-20 09:34:14 +0100

    Rechenfehler verbessert

--- src/arrays/Aufgabe1.java
+++ src/arrays/Aufgabe1.java
@@ -8,12 +8,14 @@ public class Aufgabe1 {
 	int[] dailyGains = {1000,2000,500,9000,9010};
 			
 		public int count5PercentJumps(int[]gains){
-			
 			int jumps = 0;
 			for(int i = 0; i < gains.length; i++) {
-				if((gains[i] - gains[1])  > (gains[i - 1] * 0.05)) {
+				if((gains[i] - gains[1])  > (gains[i - 1] * 1.05)) {
 					jumps++;
 				}
+			}
+			if(gains == null || gains.length < 2) {
+				return -1;
 			}
 			return 0;
 		}
