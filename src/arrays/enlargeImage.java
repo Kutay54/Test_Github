c089257e5d48553f3842c77c8d3f1118175b5a7a
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-02-26 11:44:52 +0100

    Methode erweitern

---
+++ src/arrays/enlargeImage.java
@@ -0,0 +1,32 @@
+package arrays;
+
+public class enlargeImage {
+	
+	
+	int[][] matrix = new int [2][3];
+	
+	
+	int[][]magnify(int[][]array, int factor) {
+		int factor1 = 2;
+		int newWidth = array[0].length * factor1;
+		int newLength = array[0].length * factor1;
+		int[][] newMatrix = new int[newLength][newWidth];
+		
+		int value = 1;
+		for(int i = 0; i < newWidth; i++) {
+			for(int j = 0; j < newLength; j++) {
+				newMatrix[i][j] = value;
+				value++;
+			}
+		}
+		return newMatrix;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
