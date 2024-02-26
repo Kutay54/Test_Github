e56af0d741739d3e94fdf13ece6af0872a13297a
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-02-26 11:19:20 +0100

    Aufgabe 1 fertig gemacht

--- src/arrays/Exercise1.java
+++ src/arrays/Exercise1.java
@@ -1,23 +1,28 @@
 package arrays;
 
-import java.lang.reflect.Array;
-
 public class Exercise1 {
 
-	
-	int[] dailyGains = {1000,2000,500,9000,9010};
-			
-		public int count5PercentJumps(int[]gains){
-			int jumps = 0;
-			for(int i = 0; i < gains.length; i++) {
-				if((gains[i] - gains[1])  > (gains[i - 1] * 1.05)) {
-					jumps++;
-				}
-			}
+		public static int count5PercentJumps(int[]gains){
 			if(gains == null || gains.length < 2) {
 				return -1;
 			}
-			return 0;
+			
+			int jumps = 0;
+			for(int i = 1; i < gains.length; i++) {
+				if((gains[i] - gains[i-1])  > (gains[i - 1] * 0.05)) {
+					jumps++;
+				}
+			}
+			return jumps;
+		} 
+		
+		public static void main(String[] args) {
+			
+			int[] dailyGains = {1000,2000,500,9000,9010};
+			
+			System.out.println(count5PercentJumps(dailyGains));
+			
 		}
+		
 	}
 
