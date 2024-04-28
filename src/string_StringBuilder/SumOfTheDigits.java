2965e67a15f3452ce59fcd90f3f0fb5432bf85d4
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-04-28 22:55:05 +0200

    finished Exercise 2

--- src/string_StringBuilder/SumOfTheDigits.java
+++ src/string_StringBuilder/SumOfTheDigits.java
@@ -2,7 +2,7 @@ package string_StringBuilder;
 
 import java.util.LinkedList;
 
-public class SumOfTheDigits {
+public class SumOfTheDigits {	
 	
 	public static int digitSum(long value) {
 		String str = Long.toString(value);
@@ -19,9 +19,24 @@ public class SumOfTheDigits {
 		return stack.pop();
 	}
 	
+	public static int digitSum(String value) {
+		LinkedList<Integer> stack = new LinkedList<>();		
+		for (String s : value.split("")) {
+			stack.push(Integer.parseInt(s));
+		}
+		while (!(stack.size() == 1)) {
+			int num1 = stack.pop();
+			int num2 = stack.pop();
+			int result = num1 + num2;
+			stack.push(result);
+		}
+		return stack.pop();
+	}
+	
 	
 	public static void main(String[] args) {		
 		System.out.println(digitSum(31));
+		System.out.println(digitSum("31"));
 	}
 
 }
