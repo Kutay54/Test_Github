193d82d64f49fff8200140a3ad27b701bb9968da
Kutay54 <Kutay.Tuzcu@hak-reutte.ac.at>
2024-04-29 10:28:41 +0200

    finished Exercise 4

---
+++ src/string_StringBuilder/CamelCaseSplitter.java
@@ -0,0 +1,26 @@
+package string_StringBuilder;
+
+public class CamelCaseSplitter {
+
+	public static String camelCaseSplitter(String str) {
+	 	StringBuilder sb = new StringBuilder();
+	 	for (int i = 0; i < str.length(); i++) {
+	 		char c = str.charAt(i);
+	 		if ( i > 0 && Character.isUpperCase(c) && !Character.isUpperCase(str.charAt(i - 1))) {
+	 			sb.append(' ');
+	 		}
+	 		sb.append(c);
+	 	}
+	 	return sb.toString();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(camelCaseSplitter("numberOfElement"));
+		System.out.println(camelCaseSplitter("DieseaufgabeIstErledigt"));
+
+	}
+
+}
